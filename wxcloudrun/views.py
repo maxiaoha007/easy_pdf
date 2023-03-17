cef4deefae974ea01e3f24f47637bfbff2cf7d16
maxia <307640774@qq.com>
2023-03-17 11:28:29 +0800

    文件上传

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -112,7 +112,7 @@ def get_download_url(fileid):
             {'fileid': fileid, 'max_age': 86400}
         ]
     }
-    data = json.dumps(data).encode('utf-8')
+    data = json.dumps(data, encoding='gbk',ensure_ascii=False)
     current_app.logger.info('data:%s' % data)
     # data = urllib.parse.urlencode(data).encode('utf-8')
     response = requests.post(url=url, headers=head, data=data)
@@ -132,8 +132,9 @@ def get_fileid(filename):
         "env": "prod-6gifok82d52efeb7",
         "path": filename
     }
+    data = json.dumps(data, encoding='gbk', ensure_ascii=False)
 
-    response = requests.post(url=url, headers=head, data=json.dumps(data).encode('utf-8'))
+    response = requests.post(url=url, headers=head, data=data)
     # current_app.logger.info('response:%s' % response)
     fileid = eval(response.text.encode('utf-8').decode('unicode_escape')).get('file_id')
     current_app.logger.info('fileid:%s' % fileid)
